4662b0cc729f11bec96c9705d2c6ccacbdc9a62f
Sean Murphy <sean@iamseanmurphy.com>
2024-10-03 16:19:57 +0000

    1.2.2

---
+++ icon-separator/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '6930a5481f4ac2abbaf5');
